6176a8910c0e424ca83a476cc5cd844f3923b2c2
Ariel Gonzalez <ariecgon@amazon.com>
2024-04-15 21:16:56 -0300

    update modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -76,6 +76,11 @@ export default function Modal({ showModal, onModalClose, playerName }: ModalProp
           type="submit"
           className="text-white inline-flex w-full justify-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >Agregar puntos</button>
+        <button
+          onClick={handleDialogClose}
+          type="button"
+          className="mt-4 text-white inline-flex w-full justify-center focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center  dark:focus:opacity-80"
+          >Cancelar</button>
       </form> 
 		</dialog>
 	);
