82b683f84e86b1d4a0d5170384b9a215d859baa8
Ariel Gonzalez <ariecgon@amazon.com>
2024-04-15 23:23:33 -0300

    extract toast

--- src/components/GameContainer.tsx
+++ src/components/GameContainer.tsx
@@ -3,6 +3,7 @@ import confetti from 'canvas-confetti'
 
 import Table from './Table.tsx';
 import Modal from './Modal.tsx';
+import Toast from './Toast.tsx';
 
 export interface PlayersMap {
 	[key: number]: number[];
@@ -91,29 +92,7 @@ export default function GameContainer() {
       <h1 className='my-4'>10.000 Scorer</h1>
 			<Table playersNames={playersNames} playersMap={playersMap} displayModalForPlayer={displayModalForPlayer}/>
 			<Modal showModal={showModal} onModalClose={onCloseModal} playerName={playersNames[currentPlayer!]} />
-      <div
-        id="toast-warning"
-        className={`fixed bottom-10
-          flex items-center
-          w-full max-w-xs
-          p-4
-          text-gray-500
-          bg-white
-          rounded-lg
-          shadow
-          dark:text-gray-400
-          dark:bg-gray-800
-          ${showToast ? 'opacity-100' : 'opacity-0'}
-          transition-opacity ease-in-out delay-150 duration-500`}
-        role="alert"
-      >
-        <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-orange-500 bg-orange-100 rounded-lg dark:bg-orange-700 dark:text-orange-200">
-          <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM10 15a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm1-4a1 1 0 0 1-2 0V6a1 1 0 0 1 2 0v5Z"/>
-          </svg>
-        </div>
-        <div className="ms-3 text-base font-normal">¡Te pasaste de 10.000!</div>
-      </div>
+      <Toast showToast={showToast} content="¡Te pasaste de 10.000!"/>
 		</>
 	);
 }
---
+++ src/components/Toast.tsx
@@ -0,0 +1,41 @@
+interface ToastProps {
+    showToast: boolean;
+    content: string;
+}
+
+export default function Toast({ showToast, content }: ToastProps) {
+    return (
+      <div
+        id="toast-warning"
+        className={`fixed bottom-10
+          flex items-center
+          w-full max-w-xs
+          p-4
+          text-gray-500
+          bg-white
+          rounded-lg
+          shadow
+          dark:text-gray-400
+          dark:bg-gray-800
+          ${showToast ? 'opacity-100' : 'opacity-0'}
+          transition-opacity ease-in-out delay-150 duration-500`}
+        role="alert"
+      >
+        <div
+          className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-orange-500 bg-orange-100 rounded-lg dark:bg-orange-700 dark:text-orange-200"
+        >
+          <svg
+            className="w-5 h-5"
+            aria-hidden="true"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+          >
+            <path
+              d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM10 15a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm1-4a1 1 0 0 1-2 0V6a1 1 0 0 1 2 0v5Z"
+            ></path>
+          </svg>
+        </div>
+        <div className="ms-3 text-base font-normal">{content}</div>
+      </div>
+    );
+};
