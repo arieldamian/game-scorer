a80193ad9c56f7785afd87964788f9e1cfe6c23b
Ariel Gonzalez <ariecgon@amazon.com>
2024-04-15 21:17:05 -0300

    Include toast

--- src/components/GameContainer.tsx
+++ src/components/GameContainer.tsx
@@ -11,10 +11,14 @@ export interface PlayerNamesInterface {
 }
 
 const playersNames: PlayerNamesInterface = {
-  1: 'Player 1',
-  2: 'Player 2',
-  3: 'Player 3',
-  4: 'Player 4',
+  1: 'Tota',
+  2: 'Poncho',
+  3: 'Dudi',
+  4: 'Agus',
+  5: 'Hugo',
+  6: 'Marita',
+  7: 'Nilda',
+  8: 'Raul',
 };
 
 const generatePlayersMap = (players: PlayerNamesInterface): PlayersMap => {
@@ -30,6 +34,7 @@ const generatePlayersMap = (players: PlayerNamesInterface): PlayersMap => {
 export default function GameContainer() {
 	const [currentPlayer, setCurrentPlayer] = useState<number | null>(null);
   const [showModal, setShowModal] = useState(false);
+  const [showToast, setShowToast] = useState(false);
 	const [playersMap, setPlayersMap] = useState<PlayersMap>(generatePlayersMap(playersNames));
 
 	const addScore = (playerId: number, score: number) => {
@@ -40,18 +45,38 @@ export default function GameContainer() {
 		const lastScore = playerScores[playerScores.length - 1] || 0;
 		const newScore = lastScore + score;
 
+    if (newScore > 10000) {
+      setShowToast(true);
+      setTimeout(() => {
+        setShowToast(false);
+      }, 3000);
+      return;
+    }
+
+    if (newScore === 10000) {
+      // If the score is 10000, add it to the array and finish the game
+      const newPlayersMap = {
+        ...playersMap,
+        [playerId]: [...playerScores, newScore],
+      }
+
+      setPlayersMap(newPlayersMap);
+      setCurrentPlayer(null);
+      return;
+    }
+
 		const newPlayersMap = {
 			...playersMap,
 			[playerId]: [...playerScores, newScore],
 		}
 
 		setPlayersMap(newPlayersMap);
-    	setCurrentPlayer(null);
+    setCurrentPlayer(null);
 	}
 
 	const displayModalForPlayer = (playerId: number) => {
-		setShowModal(true);
 		setCurrentPlayer(playerId);
+		setShowModal(true);
 	}
 
 	const onCloseModal = (value: number) => {
@@ -64,9 +89,32 @@ export default function GameContainer() {
 
 	return (
 		<>
-      <h1>10.000 Scorer</h1>
+      <h1 className='my-4'>10.000 Scorer</h1>
 			<Table playersNames={playersNames} playersMap={playersMap} displayModalForPlayer={displayModalForPlayer}/>
-			<Modal showModal={showModal} onModalClose={onCloseModal} />
+			<Modal showModal={showModal} onModalClose={onCloseModal} playerName={playersNames[currentPlayer!]} />
+      <div
+        id="toast-warning"
+        className={`fixed bottom-10
+          flex items-center
+          w-full max-w-xs
+          p-4
+          text-gray-500
+          bg-white
+          rounded-lg
+          shadow
+          dark:text-gray-400
+          dark:bg-gray-800
+          ${showToast ? 'opacity-100' : 'opacity-0'}
+          transition-opacity ease-in-out delay-150 duration-500`}
+        role="alert"
+      >
+        <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-orange-500 bg-orange-100 rounded-lg dark:bg-orange-700 dark:text-orange-200">
+          <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM10 15a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm1-4a1 1 0 0 1-2 0V6a1 1 0 0 1 2 0v5Z"/>
+          </svg>
+        </div>
+        <div className="ms-3 text-base font-normal">¡Te pasaste de 10.000!</div>
+      </div>
 		</>
 	);
 }
