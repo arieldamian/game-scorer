d52e29904cb74c1fcf5c816398245c74f4eaebee
Ariel Gonzalez <ariecgon@amazon.com>
2024-04-19 00:29:23 -0300

    enhance PlayerSelector

--- src/components/PlayerSelector.tsx
+++ src/components/PlayerSelector.tsx
@@ -32,34 +32,52 @@ export default function PlayerSelector({ savePlayers }: PlayerSelectorProps) {
     savePlayers(playersObj);
   }
 
-  return (<>
-    {players.map((player, index) => (
-      <div key={index} className="flex items-center justify-between p-4 md:p-5 border-b dark:border-gray-600">
-        <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-          {player}
-        </h3>
-        <button onClick={() => handleDeletePlayer(index)} type="button" className="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white">
-          <svg className="w-3 h-3" aria-hidden="true" fill="none" viewBox="0 0 14 14">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-          </svg>
-        </button>
+  return <div className="w-full mt-20 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div className="flex flex-col items-center p-10">
+      <h1 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">10.000</h1>
+      <div className="w-40 my-2 border-b border-gray-300 dark:border-gray-600" />
+      <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">Jugadores</h5>
+      <div className="flex flex-col mt-4 md:mt-6 w-full">
+            {players.map((player, index) => (
+              <div key={index} className="flex items-center justify-between p-4 md:p-5 border-b dark:border-gray-600">
+                <h3 className="text-base font-light text-gray-900 dark:text-white">
+                  {player}
+                </h3>
+                <button onClick={() => handleDeletePlayer(index)} type="button" className="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white">
+                  <svg className="w-3 h-3" aria-hidden="true" fill="none" viewBox="0 0 14 14">
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                  </svg>
+                </button>
+              </div>
+            ))}
       </div>
-    ))}
-    <form onSubmit={handleAddPlayer}>
-      <div className="flex">
-        <input
-          type="text"
-          placeholder="Nombre"
-          onChange={handleChange}
-          value={currentPlayer}
-          className="rounded-none rounded-s-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-        <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border rounded-e-0 border-gray-300 border-s-0 rounded-e-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
-          <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-            <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
-          </svg>
-        </span>
-      </div>
-    </form>
-    <button onClick={handleStartGame} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">Comenzar juego</button>
-  </>);
+      <form className="flex flex-col items-center max-w-sm mx-auto mt-10" onSubmit={handleAddPlayer}>   
+        <div className="flex">
+          <label htmlFor="simple-search" className="sr-only">Search</label>
+          <div className="relative w-full">
+            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="none" viewBox="0 0 18 20">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 5v10M3 5a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm0 10a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm12 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0V6a3 3 0 0 0-3-3H9m1.5-2-2 2 2 2"/>
+              </svg>
+            </div>
+            <input autoFocus
+              type="text"
+              placeholder="Nombre"
+              onChange={handleChange}
+              value={currentPlayer} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+          </div>
+          <button type="submit" className="p-2.5 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            <svg className="w-4 h-4" aria-hidden="true" fill="none" viewBox="0 0 20 20">
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+            </svg>
+            <span className="sr-only">Search</span>
+          </button>
+        </div>
+        <button
+          onClick={handleStartGame}
+          className="text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 font-medium py-2 px-3 mt-4 w-full"
+        >Comenzar juego</button>
+      </form>
+    </div>
+  </div>;
 }
